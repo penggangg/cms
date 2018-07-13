f59633ff426253e5689cba9cc9e2b413d479f0dc
penggang <918800642@qq.com>
2018-07-13 18:25:12 +0800

    添加容器宽度控制

---
+++ src/components/customer/list.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>客源管理</div>
+</template>
--- src/components/public/home.vue
+++ src/components/public/home.vue
@@ -2,7 +2,7 @@
     <div class="wrapper">
         <v-head></v-head>
         <v-sidebar></v-sidebar>
-        <div class="content">
+        <div :class="[{'content-collapse':collapse},'content']">
           <router-view></router-view>
         </div>
     </div>
@@ -11,6 +11,7 @@
 <script>
 import vHead from './Header.vue'
 import vSidebar from './Sidebar.vue'
+import bus from './bus'
 export default {
     data () {
         return {
@@ -18,6 +19,20 @@ export default {
     },
     components: {
         vHead, vSidebar
+    },
+    created () {
+        // 通过 Event Bus 进行组件间通信，来折叠侧边栏
+        bus.$on('collapse', msg => {
+            this.collapse = msg
+        })
     }
 }
 </script>
+<style lang="scss" scoped>
+.content {
+    background: red;
+    position: absolute;
+    right: 0;
+    top: 70px;
+}
+</style>
